e65adc85c73ae8fdf87bb8e1a8c9d1166cfc1c03
ESDLIFE\joet2 <joetsang@esdlife.com>
2025-01-06 15:29:56 +0800

    added sample response dto

---
+++ src/main/java/com/template/core/sample/dto/SampleResponseDto.java
@@ -0,0 +1,14 @@
+package com.template.core.sample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleResponseDto {
+
+    private String message;
+
+}
