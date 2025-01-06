6174402c9083dea6e71d574ad54d2318241aa385
ESDLIFE\joet2 <joetsang@esdlife.com>
2025-01-06 15:47:15 +0800

    added comment and example of throw functional exception

--- src/main/java/com/template/core/sample/controller/SampleController.java
+++ src/main/java/com/template/core/sample/controller/SampleController.java
@@ -1,5 +1,7 @@
 package com.template.core.sample.controller;
 
+import com.template.core.core.constant.ErrorType;
+import com.template.core.core.logging.FunctionalException;
 import com.template.core.sample.dto.SampleResponseDto;
 import com.template.core.sample.service.SampleService;
 import lombok.AllArgsConstructor;
@@ -12,9 +14,16 @@ public class SampleController {
 
     private final SampleService sampleService;
 
+    // This method is used to test the SampleService
     @GetMapping("/hello")
     public SampleResponseDto hello() {
         return sampleService.sayHello();
+    }
+
+    // This method is used to test the FunctionalException
+    @GetMapping("/404")
+    public String notFound() {
+        throw new FunctionalException(ErrorType.NOT_FOUND, "Not Found");
     }
 
 }
