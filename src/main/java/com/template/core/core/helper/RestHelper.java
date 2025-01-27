93f1bc1e570cb40863034bf7a685834c7f525cc6
ESDLIFE\joet2 <joetsang@esdlife.com>
2025-01-27 18:16:22 +0800

    asdas

---
+++ src/main/java/com/template/core/core/helper/RestHelper.java
@@ -0,0 +1,22 @@
+package com.template.core.core.helper;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+
+public interface RestHelper {
+
+    <T> T doGet(String url, Class<T> responseClass, Map<String, String> headersMap);
+
+    <T> T doGet(String url, Class<T> responseClass, Map<String, String> headersMap, boolean ignoreSSL);
+
+    <T> T doGet(String url, TypeReference<T> typeRef, Map<String, String> headersMap, boolean ignoreSSL) throws Exception;
+
+    <T> ResponseEntity<T> doGetForEntity(String url, Class<T> responseClass, Map<String, String> headersMap);
+
+    <T> ResponseEntity<T> doGetForEntity(String url, Class<T> responseClass, boolean ignoreSSL, Map<String, String> headersMap);
+
+    <T> ResponseEntity<T> doGetForEntity(String url, TypeReference<T> typeRef, boolean ignoreSSL, Map<String, String> headersMap);
+}
